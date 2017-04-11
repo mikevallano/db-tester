738e5b8b8c78a890ff410b5ffddc632a7b013d2f
mike vallano <mikevallano@gmail.com>
2017-04-10 19:27:35 -0500

    Add define_method to Critter.rb with a simple where query.

--- app/models/critter.rb
+++ app/models/critter.rb
@@ -1,3 +1,14 @@
 class Critter < ActiveRecord::Base
   belongs_to :owner
+
+  COMMON_CRIT_NAMES = ['zorro', 'mindy', 'fluffy', 'buttons']
+
+  class << self
+    COMMON_CRIT_NAMES.each do |crit_name|
+      define_method "#{crit_name}" do
+        self.where('name = ?', crit_name)
+      end
+    end
+  end
+
 end
