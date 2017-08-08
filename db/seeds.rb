124b6ace15159f28e0b2655c5978599c5ed98cee
mike vallano <mikevallano@gmail.com>
2017-08-07 19:50:24 -0500

    Add seeds for better database mucking.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,29 @@
 require 'ffaker'
+
+CRITS = %w[buttons mittons loki fluffy fido spike]
+
 50.times do
-  User.create(
-  username: FFaker::Internet.user_name,
-  email: FFaker::Internet.email,
-  password: FFaker::Internet.password,
-  confirmed_at: 2.days.ago)
+  user = User.create(
+                      username: FFaker::Internet.user_name,
+                      email: FFaker::Internet.email,
+                      password: FFaker::Internet.password,
+                      confirmed_at: 2.days.ago)
+  puts "created user: #{user.username}"
 end
 
 50.times do
-  Owner.create(
-    name: FFaker::Name.name,
-    description: FFaker::Job.title)
+  owner = Owner.create(
+                        name: FFaker::Name.name,
+                        description: FFaker::Job.title)
+  puts "created owner: #{owner.name}"
+end
+
+50.times do |i|
+  crit = Critter.create(
+                        name: "crit #{CRITS.sample} #{i}",
+                        description: "is a critter",
+                        owner_id: Owner.all.sample.id,
+                        weight: (5..35).to_a.sample,
+                        has_collar: [true, false].sample)
+  puts "created critter: #{crit.name}"
 end
