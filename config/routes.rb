12ffcdb3c2e051e4d0af92ea13f2b87a1f4d98d6
mike vallano <mikevallano@gmail.com>
2017-08-20 10:55:30 -0500

    Add products to front end.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
+  root 'pages#home'
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
   resources :owners
   resources :tacos
   resources :critters
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :products
 
   resources :users, only: [:show], as: :user
 
-  root 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'pages/awaiting_confirmation', as: :awaiting_confirmation
   get 'dump_download', to: 'pages#dump_download', as: :dump_download
